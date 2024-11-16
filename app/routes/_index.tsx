fc7d6b7ad44281b9804ea2d41edccdd0622ca924
AzureAD\AndreaCadavid <andreacadavidosorio@gmail.com>
2024-11-16 17:09:17 +1100

    Update main route and add images to public folder

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
+import Counter from "~/components/counter";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,16 +12,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <NextUIProvider>
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col items-center gap-16">
-          <header className="flex flex-col items-center gap-9">
-            <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-              Welcome to <span>Volleytip</span>
-            </h1>
-          </header>
-          <Link to="/counter">Go to score counter</Link>
-        </div>
-      </div>
+      <Counter/>
     </NextUIProvider>
   );
 }
